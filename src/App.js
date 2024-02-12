521f7f23015170d12739238d3ceb34cfcf21d4b7
dhreda <redoxplayer3@gmail.com>
2024-02-13 00:45:14 +0100

    modifications

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
-import "./App.css"
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Country from "./components/Country";
 import Home from "./components/Home";
 
 const App = () => {
   const [theme, setTheme] = useState("light");
-  const themeColor = (color)=> {
-    setTheme(color)
-  }
+  const themeColor = (color) => {
+    setTheme(color);
+  };
   return (
     <div className="app" mode={theme}>
-      <BrowserRouter>
-        <Header switchColor={themeColor} currentColor={theme}/>
+      <BrowserRouter basename="api-countries-infos">
+        <Header switchColor={themeColor} currentColor={theme} />
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home />} />
           <Route path="/countries">
-            <Route index element={<Home/>}/>
-            <Route path=":name" element={<Country/>}/>
+            <Route index element={<Home />} />
+            <Route path=":name" element={<Country />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 };
 
 export default App;
